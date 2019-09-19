4d62353b78a3a4910c5702375d067a05482f173b
Aaron Vuong <aaron.vuong@hotmail.com>
2019-09-19 16:53:03 -0400

    Final Python

--- udpReceiver.py
+++ udpReceiver.py
@@ -9,6 +9,10 @@ port = int(textport)
 server_address = ('localhost', port)
 s.bind(server_address)
 
+d= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+p = 1007
+sa = ('localhost', p)
+
 while True:
 
     print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
@@ -17,5 +21,8 @@ while True:
     if not len(buf):
         break
     print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
+    
+    data = "ACK:" + str(buf)
+    d.sendto(data.encode('utf-8'), sa)
 
 s.shutdown(1)
--- udpSender.py
+++ udpSender.py
@@ -4,18 +4,34 @@ import socket, sys, time
 
 host = sys.argv[1]
 textport = sys.argv[2]
+n = sys.argv[3]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
 
-while 1:
-    print ("Enter data to transmit: ENTER to quit")
-    data = sys.stdin.readline().strip()
-    if not len(data):
-        break
-#    s.sendall(data.encode('utf-8'))
-    s.sendto(data.encode('utf-8'), server_address)
+d= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+p = 1007
+sa = ('localhost', p)
+d.bind(sa)
 
-s.shutdown(1)
+
+i = 1
+while 1:
+    
+    while(i<=int(n)):  
+        data = "Message " + str(i)
+        if not len(data):
+            break
+#    s.sendall(data.encode('utf-8'))
+        s.sendto(data.encode('utf-8'), server_address)
+        while True:
+            buf, address = d.recvfrom(port)
+            if not len(buf):
+                break
+            print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
+            break
+        i= i + 1
+    break
+    s.shutdown(1)
 
